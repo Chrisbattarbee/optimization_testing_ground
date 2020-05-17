86ca7c74006476fd63355e1eea705b8b2d799eef
Chris Battarbee <chris@chrisbattarbee.com>
2020-05-17 22:19:55 +0100

    Initial hardware prefetching benchmark implementation

--- clusteredness/prefetching.cpp
+++ clusteredness/prefetching.cpp
@@ -1,24 +1,58 @@
 #include <benchmark/benchmark.h>
 #include "prefetching.h"
 
+static void* flush_data_cache() {
+    const int size = 40*1024*1024; // Allocate 40M. Much larger than L3 cache
+    char *c = (char *)malloc(size);
+    for (int i = 0; i < 0xffff; i++) {
+        for (int j = 0; j < size; j++) {
+            c[j] = i * j;
+        }
+    }
+    return c;
+}
+
+/*
+ * This benchmark aims to show the speedup provided by hardware prefetching on a per element read basis
+ * Higher amounts of memory should have an overall higher throughput of the read due to the predictable stride
+ * access pattern and the hardware prefetcher kicking in.
+ */
 #define NUM_ITERATIONS 100000000
 static void BM_HardwarePrefetching(benchmark::State& state) {
     // Setup
+    srand(time(NULL));
+    auto num_elements = state.range(0);
+    auto* array = (uint32_t*) malloc(sizeof(u_int32_t) * num_elements);
+    for (uint32_t x = 0; x < num_elements; x ++) {
+        array[x] = rand();
+    }
+
+    void* cache_flush_data = flush_data_cache();
 
     // Actual benchmark
     for (auto _ : state) {
         volatile __uint32_t total = 0;
-
+        for (int x = 0; x < num_elements; x ++) {
+            total += array[x];
+        }
     }
 
     // Teardown
+    free(array);
+    free(cache_flush_data);
 }
 
-// Provides arguments of the cross product of [0..101, 10] x [0..101, 10]
+#define MAX_NUM_ELEMENTS_IN_ARRAY 100000
+// Provides arguments in the range 1..100000, skewing the produced arguments towards 0
 static void CustomArguments(benchmark::internal::Benchmark* b) {
-    for (int i = 0; i <= 100; i+= 10)
-        for (int j = 0; j <= 100; j+= 10)
-            b->Args({i, j});
+    for (int i = 0; i < 100; i+= 10)
+        b->Args({i});
+    for (int i = 100; i < 1000; i+= 100)
+        b->Args({i});
+    for (int i = 1000; i < 10000; i+= 1000)
+        b->Args({i});
+    for (int i = 10000; i < MAX_NUM_ELEMENTS_IN_ARRAY; i+= 10000)
+        b->Args({i});
 }
 
 void prefetching::register_benchmarks() {
--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <benchmark/benchmark.h>
 #include "clusteredness/clusteredness.h"
+#include "clusteredness/prefetching.h"
 
 int main(int argc, char *argv[]) {
     clusteredness::register_benchmarks();
+    prefetching::register_benchmarks();
     benchmark::Initialize(&argc, argv);
     benchmark::RunSpecifiedBenchmarks();
 }
