7df759ddf5c72ca0f78720a8d3ffa3fae47e8247
Chris Battarbee <chris@chrisbattarbee.com>
2020-05-18 01:59:49 +0100

    Fix benchmark

--- clusteredness/prefetching.cpp
+++ clusteredness/prefetching.cpp
@@ -5,7 +5,7 @@ static void* flush_data_cache() {
     const int size = 40*1024*1024; // Allocate 40M. Much larger than L3 cache
     char *c = (char *)malloc(size);
     for (int j = 0; j < size; j++) {
-        benchmark::DoNotOptimize(c[j] = rand());
+        benchmark::DoNotOptimize(c[j] = 0);
     }
     return c;
 }
@@ -15,7 +15,6 @@ static void* flush_data_cache() {
  * Higher amounts of memory should have an overall higher throughput of the read due to the predictable stride
  * access pattern and the hardware prefetcher kicking in.
  */
-#define NUM_ITERATIONS 100000000
 static void BM_HardwarePrefetching(benchmark::State& state) {
     // Setup
     srand(time(NULL));
@@ -25,25 +24,62 @@ static void BM_HardwarePrefetching(benchmark::State& state) {
         array[x] = rand();
     }
 
-    void* cache_flush_data = flush_data_cache();
-
     // Actual benchmark
     for (auto _ : state) {
-        __uint32_t total = 0;
-        for (int x = 0; x < num_elements; x ++) {
+        state.PauseTiming();
+        free(flush_data_cache());
+        state.ResumeTiming();
+        volatile __uint32_t total = 0;
+        for (int x = 0; x < num_elements; x += 4) {
             benchmark::DoNotOptimize(total += array[x]);
         }
     }
 
     // Teardown
     free(array);
-    free(cache_flush_data);
+}
+
+#define INDEX_ARRAY_SIZE 128
+static void BM_LackOfHardwarePrefetching(benchmark::State& state) {
+    // Setup
+    srand(time(NULL));
+    auto num_elements = state.range(0);
+    auto* array = (uint32_t*) malloc(sizeof(u_int32_t) * num_elements);
+    for (uint32_t x = 0; x < num_elements; x ++) {
+        array[x] = rand();
+    }
+
+    auto* index_array = (uint32_t*) malloc(sizeof(u_int32_t) * INDEX_ARRAY_SIZE);
+    for (uint32_t x = 0; x < INDEX_ARRAY_SIZE; x ++) {
+        index_array[x] = rand() % num_elements;
+    }
+
+    // Actual benchmark
+    for (auto _ : state) {
+        state.PauseTiming();
+        free(flush_data_cache());
+        volatile uint32_t temp = 0;
+        // Load our index array in after we flush cache
+        for (int x = 0; x < INDEX_ARRAY_SIZE; x ++) {
+            benchmark::DoNotOptimize(temp = index_array[x]);
+        }
+        state.ResumeTiming();
+        volatile __uint64_t total = 0;
+        for (int x = 0; x < num_elements; x ++) {
+            benchmark::DoNotOptimize(total += array[index_array[x % INDEX_ARRAY_SIZE]]);
+        }
+    }
+
+    // Teardown
+    free(array);
 }
 
 #define MAX_NUM_ELEMENTS_IN_ARRAY 100000
 // Provides arguments in the range 1..100000, skewing the produced arguments towards 0
 static void CustomArguments(benchmark::internal::Benchmark* b) {
-    for (int i = 0; i < 100; i+= 10)
+    for (int i = 1; i < 10; i+= 1)
+        b->Args({i});
+    for (int i = 10; i < 100; i+= 10)
         b->Args({i});
     for (int i = 100; i < 1000; i+= 100)
         b->Args({i});
@@ -54,5 +90,6 @@ static void CustomArguments(benchmark::internal::Benchmark* b) {
 }
 
 void prefetching::register_benchmarks() {
-    BENCHMARK(BM_HardwarePrefetching)->Apply(CustomArguments);
+    BENCHMARK(BM_HardwarePrefetching)->Apply(CustomArguments)->Iterations(100);
+    BENCHMARK(BM_LackOfHardwarePrefetching)->Apply(CustomArguments)->Iterations(100);
 }
